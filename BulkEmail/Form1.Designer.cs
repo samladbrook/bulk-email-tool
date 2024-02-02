ad171878c1826e8a845a7fecb07b5a177c2e0136
Samuel Ladbrook <samladbrook@outlook.com>
2024-02-02 14:10:12 +1300

    Add project files.

---
+++ BulkEmail/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BulkEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBoxPassSH = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(80, 99);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(80, 47);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(207, 20);
+            this.textBoxEmail.TabIndex = 1;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(80, 73);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(207, 20);
+            this.textBoxPass.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // checkBoxPassSH
+            // 
+            this.checkBoxPassSH.AutoSize = true;
+            this.checkBoxPassSH.Location = new System.Drawing.Point(294, 75);
+            this.checkBoxPassSH.Name = "checkBoxPassSH";
+            this.checkBoxPassSH.Size = new System.Drawing.Size(86, 17);
+            this.checkBoxPassSH.TabIndex = 5;
+            this.checkBoxPassSH.Text = "Show / Hide";
+            this.checkBoxPassSH.UseVisualStyleBackColor = true;
+            this.checkBoxPassSH.CheckedChanged += new System.EventHandler(this.checkBoxPassSH_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBoxPassSH);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBoxPassSH;
+    }
+}
+
---
+++ BulkEmail/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+namespace BulkEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string emailAdd = textBoxEmail.Text;
+            string pass = textBoxPass.Text;
+            string accCode = "MAKI";
+            string accName= "Makita";
+            string accEmail = "makita@gmail";
+            string firstName = "Makita";
+
+
+            MimeMessage email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress("Sam Ladbrook", emailAdd));
+            email.To.Add(new MailboxAddress("Craig Ladbrook", "craig.ladbrook@mitre10.co.nz"));
+            email.To.Add(new MailboxAddress("Craig Ladbrook", "craig.ladbrook@mitre10.co.nz"));
+
+            email.Subject = "Test email";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = $@"<p>Dear {firstName},</p>
+                        <p>Account Code: {accCode}</p>
+                        <p>Account Name: {accName}</p>
+                        <p>Account Email: {accEmail}</p>
+                        <p>Over the next 3 months, Taupo Mitre 10 MEGA is migrating to a new software system called SAP.</p>
+                        <p>With this new system being introduced, we now have certain new criteria for customer accounts,
+                        in-particular around minimum monthly dollar spend which is now set at $500.00 a month.
+                        Due to your current spend not meeting this criteria we have had to de-activate your account prior to switching over to the new system.
+                        If you believe your spending may change either now or in the future, please feel free to make contact with us to discuss options,
+                        which we are more than happy to do as we certainly value your business.</p>
+                        <p>We apologize if this causes any inconvenience but again we are happy to discuss if your circumstances change.
+                        If this is the case, in the first instance, you can speak to the team at Customer Services who will take all your details.</p>
+                        <p>Thanks again for your continued business.</p>
+                        <p>Your Sincerely,</p>
+                        <p>Craig Ladbrook</p>
+                        <p>Director / Member Principal</p>
+                        <p>M: 027 555 3338   E: craig.ladbrook@mitre10.co.nz</p>
+                        <p>Mitre 10 Mega Taupo</p>
+                        <p>99 Bella George Lane, Taupo 3379 | PO Box 824, Taupo, 3351</p>"
+            };
+            
+
+            using (SmtpClient smtpClient = new SmtpClient())
+            {
+                //smtpClient.Connect("209.133.218.2", 587, false);
+                smtpClient.Connect("smtp.office365.com", 587, false);
+
+                
+                smtpClient.Authenticate(emailAdd, pass);
+
+                smtpClient.Send(email);
+                MessageBox.Show("the message should be sent");
+
+
+                smtpClient.Disconnect(true);
+                
+            }
+        }
+
+        private void checkBoxPassSH_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPassSH.Checked)
+            {
+                textBoxPass.UseSystemPasswordChar = PasswordPropertyTextAttribute.No.Password;
+            }
+            else
+            {
+                textBoxPass.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxPass.UseSystemPasswordChar = PasswordPropertyTextAttribute.Yes.Password;
+
+        }
+    }
+}
